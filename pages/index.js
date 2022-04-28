329f8078351166e1142157efb47c9b38e268fb0a
HangCcz <hangchen1128@gmail.com>
2022-04-27 23:47:37 -0400

    renamed title to make it fancy

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ import Footer from '../components/Footer';
 const Home = () => (
   <div className="text-gray-700 bg-gray-100">
     <Head>
-      <title>Hang's Italian Kitchen</title>
+      <title>Hang's Very Fancy Italian Restaurant</title>
       <link rel="icon" href="/pizza.png" />
     </Head>
     <div className="mx-auto max-w-3xl ">
       <main className="flex flex-col px-8 pt-4 text-center">
         <h1 className="text-3xl text-red-400 mb-8 uppercase">
-          <span className="text-4xl">Hang's </span>Italian Kitchen
+          <span className="text-4xl">Hang's </span>Very Fancy Italian Restaurant
         </h1>
         <Menu />
       </main>
